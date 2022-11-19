349ab701b0791588ee017b68e0087534d6106e13
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-18 23:37:05 -0800

    Fetch more secrets

--- app/env.py
+++ app/env.py
@@ -31,6 +31,10 @@ class ConfigGenerator:
             'DB_PASS': self.get_parameter('/mastodon/postgres/password'),
             'SMTP_LOGIN': self.get_parameter('/mastodon/ses/username'),
             'SMTP_PASSWORD': self.get_parameter('/mastodon/ses/password'),
+            'VAPID_PUBLIC_KEY': self.get_parameter('/mastodon/vapid/public_key'),
+            'VAPID_PRIVATE_KEY': self.get_parameter('/mastodon/vapid/private_key'),
+            'SECRET_KEY_BASE': self.get_parameter('/mastodon/secret_key_base'),
+            'OTP_SECRET': self.get_parameter('/mastodon/otp_secret'),
         }
         env.globals['secrets'] = secrets
 
