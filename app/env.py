4aa6c691bfcd208e1a7efb1b44c180804d2b8246
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-18 23:08:31 -0800

    Retrieve secrets from SSM

---
+++ app/env.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import argparse
+from pathlib import Path
+
+import jinja2
+from boto3 import client
+
+
+class ConfigGenerator:
+
+    def __init__(self):
+        self.ssm = client('ssm')
+
+    def get_parameter(self, name: str):
+        resp = self.ssm.get_parameter(
+            Name=name,
+            WithDecryption=True
+        )
+        value = resp['Parameter']['Value']
+        return value
+
+    def gen(self, src_dir: Path, dst_dir: Path):
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(src_dir),
+            undefined=jinja2.StrictUndefined
+        )
+        secrets = {
+            'REDIS_HOST': self.get_parameter('/mastodon/redis/address'),
+            'DB_HOST': self.get_parameter('/mastodon/postgres/endpoint'),
+            'DB_PASS': self.get_parameter('/mastodon/postgres/password'),
+            'SMTP_LOGIN': self.get_parameter('/mastodon/ses/username'),
+            'SMTP_PASSWORD': self.get_parameter('/mastodon/ses/password'),
+        }
+        env.globals['secrets'] = secrets
+
+        for tpl in src_dir.glob('*.j2'):
+            template = env.get_template(tpl.name)
+            output_path = dst_dir / tpl.name.replace('.j2', '')
+            output_content = template.render()
+            output_path.write_text(output_content)
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('src_dir', type=Path)
+    ap.add_argument('dst_dir', type=Path)
+    args = ap.parse_args()
+
+    generator = ConfigGenerator()
+    generator.gen(args.src_dir, args.dst_dir)
+
+
+if __name__ == '__main__':
+    main()
